15702151e3cf4a903211cd9278679ef2d084d46f
Bekanzar007 <saitovbeknazar6@gmail.com>
2022-06-03 02:00:54 +0600

    backend part

--- src/main/java/com/example/demo/controller/UsersController.java
+++ src/main/java/com/example/demo/controller/UsersController.java
@@ -16,22 +16,27 @@ public class UsersController {
         this.usersService = usersService;
     }
 
-    @RequestMapping("/register")
+    @GetMapping("/register")
     public String getRegisterPage(Model model)  {
         model.addAttribute("registerRequest", new UsersModel());
         return "register_page";
     }
-    @RequestMapping("/login")
+
+    @GetMapping("/login")
     public String getLoginPage(Model model)  {
         model.addAttribute("loginRequest", new UsersModel());
         return "login_page";
     }
+
+
+
     @PostMapping("/register")
     public String rigister(@ModelAttribute UsersModel usersModel){
         System.out.println("register request: " + usersModel);
         UsersModel registeredUser = usersService.registerUser(usersModel.getLogin(), usersModel.getPassword(), usersModel.getEmail());
         return registeredUser == null ? "error_page" : "redirect:/login";
     }
+
     @PostMapping("/login")
     public String login(@ModelAttribute UsersModel usersModel){
         System.out.println("register request: " + usersModel);
