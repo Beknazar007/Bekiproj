437cb6ee18ddc1c3505caab3488304843a871854
Bekanzar007 <saitovbeknazar6@gmail.com>
2022-06-03 08:22:57 +0600

    adding template of site

--- src/main/java/com/example/demo/controller/UsersController.java
+++ src/main/java/com/example/demo/controller/UsersController.java
@@ -50,7 +50,7 @@ public class UsersController {
         System.out.println("login request: " + usersModel);
         UsersModel authenticateUser = usersService.authenticate(usersModel.getLogin(), usersModel.getPassword());
     if (authenticateUser != null){
-     return "order_page";
+     return "index";
     }else{
         return "error_page";
     }
