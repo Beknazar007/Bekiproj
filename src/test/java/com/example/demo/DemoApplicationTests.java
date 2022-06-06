de9c0e6712038b7333a96f172da349a4df51b8be
Bekanzar007 <saitovbeknazar6@gmail.com>
2022-06-07 00:02:24 +0600

    Jenkins with test

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,42 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	@Autowired
+	private MockMvc mockMvc;
+	@Test
+	public void getsAllRides() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/login")
+						.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andReturn();
+	}
+	@Test
+	public void getsSingleRide() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/register")
+						.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andReturn();
+	}
+	@Test
+	public void returnsNotFoundForInvalidSingleRide() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/login/2")
+						.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isNotFound())
+				.andReturn();
 	}
 
 }
